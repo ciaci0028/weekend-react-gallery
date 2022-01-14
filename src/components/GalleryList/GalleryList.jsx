461cf5de5b1f4a1f83bb40c951f077f08b972c97
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-14 08:57:42 -0600

    successfully sending data to gallery list component

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,16 @@
+function GalleryList (galleryList) {
+    console.log('gallery list', galleryList.galleryList);
+    return (
+        <>
+            <h1>Gallery is going to go here</h1>
+                {/* <div key={pic.id}>
+                    {galleryList.galleryList.map(pic => (
+                    {pic.description}
+                    {pic.likes}
+                ))};
+                </div> */}
+        </>
+    )
+};
+
+export default GalleryList;
